97f0c3b1a6d9431517eedc8ad0d7d22a6e6ad3f0
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2019-02-23 11:53:47 -0300

    init some 15

---
+++ Soma_15/sketch.js
@@ -0,0 +1,40 @@
+var tab =
+[
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+/*
+    C0 C1 C2
+ l0  1  2  3
+ l1  4  5  6
+ l2  7  8  9
+*/
+
+
+
+function windowResized ()
+{
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup ()
+{
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw ()
+{
+  draw_table();
+}
+
+function draw_table ()
+{
+  for(var i = 0; i < 3; i++)
+  {
+    for (var j = 0; j < 3; j++)
+    {
+
+    }
+  }
+}
