3475e3ab29dd6a947882c7a0fa2eeec44e44dbce
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2019-03-01 10:17:38 -0300

    draw table :)

--- Soma_15/sketch.js
+++ Soma_15/sketch.js
@@ -11,30 +11,49 @@ var tab =
  l2  7  8  9
 */
 
-
-
-function windowResized ()
-{
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-function setup ()
-{
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw ()
-{
-  draw_table();
-}
-
 function draw_table ()
 {
   for(var i = 0; i < 3; i++)
   {
     for (var j = 0; j < 3; j++)
     {
+      //console.log(i, j, "valor: ", tab[i][j]);
+    }
+  }
+}
 
+function setup ()
+{
+  createCanvas(300, 300);
+
+  colorMode(RGB);
+}
+
+function windowResized ()
+{
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw ()
+{
+  background(71, 207, 212);
+
+  for(var i = 0; i < 3; i++)
+  {
+    for (var j = 0; j < 3; j++)
+    {
+      push();
+        translate(j*100, i*100);
+
+        fill(237, 237, 237);
+        strokeWeight(2);
+        rect(0, 0, 100, 100);
+
+        fill(30, 30, 30);
+        textSize(30);
+        textAlign(CENTER, CENTER);
+        text(tab[i][j], 50, 50);
+      pop();
     }
   }
 }
